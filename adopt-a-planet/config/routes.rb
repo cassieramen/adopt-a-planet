f62c47c242af3bdccfe14505fd7eb463d66f8b9c
Elijah <kimelijah90@gmail.com>
2014-11-08 02:55:02 -0500

    Added a show page for a star
    *Was also able to populate rails mongo db

--- adopt-a-planet/config/routes.rb
+++ adopt-a-planet/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :home, only: :index
-  resources :planets, only: [:edit, :update]
+  resources :planets, only: [:show, :edit, :update]
 
   root to: "home#index"
 
